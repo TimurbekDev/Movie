872c2cd51ea032ae75f93f5ca9bf9934ab24aac3
Nodirbek Savbatov Sherzod O'g'li <116478730+nodir0306@users.noreply.github.com>
2024-11-04 19:35:10 +0500

    test

--- src/modules/actor/actor.module.ts
+++ src/modules/actor/actor.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ActorService } from './actor.service';
 import { ActorController } from './actor.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Actor } from './entities';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Actor])],
   controllers: [ActorController],
   providers: [ActorService],
 })
